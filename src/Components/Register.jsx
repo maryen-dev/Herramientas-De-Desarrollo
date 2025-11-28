a461beaf5c4d0f67d2a8c33711eaf37cb0bb8e7b
Edson <U22330669@utp.edu.pe>
2025-11-28 15:04:47 -0500

    Register

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -58,7 +58,7 @@ export default function Register() {
       rol: form.role,
     };
 
-    fetch('https://tu-backend.onrender.com/registro', {
+    fetch('https://herramientasbackend.onrender.com/registro', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: new URLSearchParams(cliente),
